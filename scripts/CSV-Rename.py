f81e931e79ac21edb07260eea91685a2b500a987
Eric Boxer <eboxhead@gmail.com>
2018-06-16 23:47:41 -0400

    Added missing rename.csv handling

--- scripts/CSV-Rename.py
+++ scripts/CSV-Rename.py
@@ -4,7 +4,12 @@ import argparse
 import logging
 import os
 import sys
-# import threading
+
+'''
+This script will rename all fo the files in the current working director based on a csv file
+'''
+
+
 
 # =====================
 # META, yo 
@@ -20,6 +25,7 @@ __author__ = "Eric Boxer"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="The CSV file that has the renameing parameters. Defaults to \"rename.csv\".", action='store')
+parser.add_argument("-g", "--generate", help="generates a blank rename.csv for you to work from", action='store_true')
 parser.add_argument("-i", "--ignore", help="Ignore the first (header) line.", action='store_true')
 parser.add_argument("-e", "--export", help="Export missing files list to csv.", action="store_true")
 parser.add_argument("-u", "--undo", help="Undo the naming.", action='store_true')
@@ -41,7 +47,6 @@ verbose = args.verbose
 
 if verbose or debug:
     print(args)
-
 # =====================
 # end Settings 
 # =====================
@@ -57,8 +62,6 @@ def addDebug(func):
         else:
             return func(*args, **kwargs)
     return func_wrapper
-
-
 # =====================
 # end Decorators 
 # =====================
@@ -76,22 +79,32 @@ def processCSV():
     missingFiles = []
     missingFilesFile = os.path.join(os.getcwd(), "missingFiles.csv")
 
+    # What file are we reading from
     if args.file:
         renameFile = args.file
     else:
         renameFile = "rename.csv"
 
-    with open(renameFile, mode = "r") as f:
 
+    # Does the rename file exist? If not why are we here?
+    if not os.path.isfile(os.path.join(os.getcwd(), renameFile)):
+        print(f"Womp womp, {renameFile} isn't here.")
+        end()
+
+    # Great. Read through the file and change names
+    with open(renameFile, mode = "r") as f:
         lineNumber = 1
         for line in f:
+
+            # Is there header? I don't really care about it.
             if lineNumber == 1 and args.ignore:
-                line.strip()
+                pass
             else:
                 splitLine = removeQuotes(line.strip()).split(',')
                 if args.verbose or args.debug:
                     print(f"[{lineNumber}] Current line: {splitLine}")
                 try:
+                    # Did we make a stupid and have to change back? We can do that!
                     if args.undo:
                         firstFile = splitLine[1]
                         secondFile = splitLine[0]
@@ -101,14 +114,14 @@ def processCSV():
                     rename(os.path.join(os.getcwd(), firstFile),os.path.join(os.getcwd(), secondFile))
                     if verbose:
                         print(f"{firstFile} renamed to {secondFile}")
+                # You tell me theres a file. But theres not.
                 except FileNotFoundError:
                     if verbose:
                         print(f"Could not find file {firstFile}")
                     missingFiles.append(firstFile)
             lineNumber += 1
-    print(missingFiles)
 
-
+    #  If there a missing files spit them out to a file
     if len(missingFiles) == 0:
         returnMessage = f"{renameFile} processed."
     else:
@@ -118,6 +131,8 @@ def processCSV():
             for line in missingFiles:
                 f.write(f"{line}\n")
         returnMessage = f"{renameFile} processed with errors."
+    
+    # This really does anygood if you're debugging
     return returnMessage
 
 def end():
