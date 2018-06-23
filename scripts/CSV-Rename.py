a18f9fe6a6ace85eaa32e6cfa8ea9566eab1a733
Eric Boxer <eboxhead@gmail.com>
2018-06-23 10:12:29 -0400

    Cleanup

--- scripts/CSV-Rename.py
+++ scripts/CSV-Rename.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
-import logging
+import csv
+import fnmatch
 import os
-import sys
 
 '''
-This script will rename all fo the files in the current working director based on a csv file
+CSV-Rename renames files based on a 'rename.csv' in the same directory
 '''
-
-
 
 # =====================
 # META, yo 
@@ -18,14 +16,13 @@ This script will rename all fo the files in the current working director based o
 __version__ = '0.0.1'
 __author__ = "Eric Boxer"
 
-
 # =====================
 # Parsing 
 # =====================
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="The CSV file that has the renameing parameters. Defaults to \"rename.csv\".", action='store')
-parser.add_argument("-g", "--generate", help="generates a blank rename.csv for you to work from", action='store_true')
+parser.add_argument("-t", "--template", help="generates a blank rename.csv for you to work from", action='store_true')
 parser.add_argument("-i", "--ignore", help="Ignore the first (header) line.", action='store_true')
 parser.add_argument("-e", "--export", help="Export missing files list to csv.", action="store_true")
 parser.add_argument("-u", "--undo", help="Undo the naming.", action='store_true')
@@ -37,8 +34,6 @@ args = parser.parse_args()
 # end Parsing 
 # =====================
 
-
-
 # =====================
 # Settings 
 # =====================
@@ -47,10 +42,18 @@ verbose = args.verbose
 
 if verbose or debug:
     print(args)
+
+missingFiles = []
+missingFilesFile = os.path.join(os.getcwd(), "missingFiles.csv")
+
+# What file are we reading from
+if args.file:
+    renameFile = os.path.join(os.getcwd(), args.file)
+else:
+    renameFile = os.path.join(os.getcwd(), "rename.csv")
 # =====================
 # end Settings 
 # =====================
-
 
 # ===============
 # Decorators
@@ -74,21 +77,34 @@ def removeQuotes(string):
     return string.replace("\"","")
 
 @addDebug
+def writeFile(file, outputData):
+    with open(file, 'w') as f:
+        csvWriter = csv.writer(f)
+        csvWriter.writerows(outputData)
+    return f"{file} has been written."
+
+@addDebug
+def makeTemplate():
+    """
+    Generates the rename file
+    """
+
+    csvTemplateData = [["Current File Name", "New File Name"]]
+
+    fileList = os.listdir(os.getcwd())
+    for item in fileList:
+        # All actual and non hidden files...
+        if os.path.isfile(os.path.join(os.getcwd(), item)) and not item.startswith('.'):
+            csvTemplateData.append([item,""])
+
+    writeFile(renameFile, csvTemplateData)
+    return f"rename.csv exported with data {csvTemplateData}"
+
+@addDebug
 def processCSV():
-
-    missingFiles = []
-    missingFilesFile = os.path.join(os.getcwd(), "missingFiles.csv")
-
-    # What file are we reading from
-    if args.file:
-        renameFile = args.file
-    else:
-        renameFile = "rename.csv"
-
-
     # Does the rename file exist? If not why are we here?
-    if not os.path.isfile(os.path.join(os.getcwd(), renameFile)):
-        print(f"Womp womp, {renameFile} isn't here.")
+    if not os.path.isfile(renameFile):
+        print(f"Womp womp, {renameFile} isn't here. Try generating with -t.")
         end()
 
     # Great. Read through the file and change names
@@ -111,14 +127,19 @@ def processCSV():
                     else:
                         firstFile = splitLine[0]
                         secondFile = splitLine[1]
-                    rename(os.path.join(os.getcwd(), firstFile),os.path.join(os.getcwd(), secondFile))
-                    if verbose:
-                        print(f"{firstFile} renamed to {secondFile}")
+                    if firstFile == '' or secondFile == '':
+                        if verbose:
+                            print(f"[{lineNumber}] excluded")
+                    else:
+                        rename(os.path.join(os.getcwd(), firstFile),os.path.join(os.getcwd(), secondFile))
+                        if verbose:
+                            print(f"{firstFile} renamed to {secondFile}")
+
                 # You tell me theres a file. But theres not.
                 except FileNotFoundError:
                     if verbose:
                         print(f"Could not find file {firstFile}")
-                    missingFiles.append(firstFile)
+                    missingFiles.append([firstFile])
             lineNumber += 1
 
     #  If there a missing files spit them out to a file
@@ -127,12 +148,8 @@ def processCSV():
     else:
         if os.path.isfile(missingFilesFile):
             os.remove(missingFilesFile)
-        with open(missingFilesFile, 'w') as f:
-            for line in missingFiles:
-                f.write(f"{line}\n")
+        writeFile(missingFilesFile,missingFiles)
         returnMessage = f"{renameFile} processed with errors."
-    
-    # This really does anygood if you're debugging
     return returnMessage
 
 def end():
@@ -142,5 +159,9 @@ def end():
 
 if __name__ == "__main__":
   
+    if args.template:
+        makeTemplate()
+        end()
+
     processCSV()
     end()
