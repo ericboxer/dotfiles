43a20ffe844038718a14eb0acf5066fde2328b05
Eric Boxer <eboxhead@gmail.com>
2018-06-24 12:20:07 -0400

    Removed files in lieu of Homebrew

--- installFonts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# created by: eric boxer 2015
-# email: eric@ericboxer.net  
-# http://ericboxer.net 
-
-#This will install all the fonts located in the fonts directory in dropbox
-
